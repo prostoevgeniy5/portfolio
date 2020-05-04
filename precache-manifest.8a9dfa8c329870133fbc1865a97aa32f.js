d54030b18f04e5cad67138489b8b227326bc714a
EvgeniyKir <yevgeniykyr2@ukr.net>
2020-05-04 08:38:40 +0300

    Updates

---
+++ precache-manifest.8a9dfa8c329870133fbc1865a97aa32f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5d59190539df53d7a9e92551019a1d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bd873951a9f7f1e907a4",
+    "url": "/portfolio/static/css/main.a1d2278d.chunk.css"
+  },
+  {
+    "revision": "da9df78071ed4035d11d",
+    "url": "/portfolio/static/js/2.732067e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.732067e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd873951a9f7f1e907a4",
+    "url": "/portfolio/static/js/main.a851575e.chunk.js"
+  },
+  {
+    "revision": "040378e5b873eb341ece",
+    "url": "/portfolio/static/js/runtime-main.f75819b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
